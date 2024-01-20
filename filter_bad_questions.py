26c484634c498f59b646b862612211e519a6dda8
Nish-19 <nischalrokz19@gmail.com>
2024-01-20 10:19:03 +0530

    update filtering

--- filter_bad_questions.py
+++ filter_bad_questions.py
@@ -5,9 +5,46 @@ Filter generated bad questions based on the classification results
 import os 
 import sys
 from tqdm import tqdm
+from ast import literal_eval
 import json 
 from collections import defaultdict
 from classifiy_gen_questions import process_questions, parse_questions
+
+def check_validity(classification_result):
+    '''
+    Check if classification result is valid
+    '''
+    valid_categories = {'Irrelevant':0, 'Repeated':0, 'Direct':0, 'Premature':0}
+
+    # extract string between curly braces ({})
+    classification_result = classification_result[classification_result.find('{'):classification_result.find('}')+1].strip()
+    classification_result_dict = literal_eval(classification_result)
+    # sort the dict based on values 
+    sorted_classification_result_dict = sorted(classification_result_dict.items(), key=lambda x: x[1], reverse=True)
+    # extract the first key
+    first_key = sorted_classification_result_dict[0][0]
+    try:
+        valid_categories[first_key] += 1
+        decision = True
+    except KeyError:
+        decision = False
+    return decision
+
+
+
+def filter_bad_questions(bad_questions_list, classification_results_list):
+    '''
+    Filter bad questions based on classification results
+    '''
+    valid_bad_questions = []
+    for question, classification_result in zip(bad_questions_list, classification_results_list):
+        # TODO: Process classification result
+        valid_result = check_validity(classification_result)
+        if valid_result:
+            valid_bad_questions.append(question)
+    
+    return valid_bad_questions
+
 
 def main():
     data_path = 'socratic-debugging-benchmark/socratic_debugging_benchmark/v2_sigcse'
@@ -47,6 +84,17 @@ def main():
             bad_questions_list = parse_questions(bad_questions)
             # assert length
             assert len(bad_questions_list) == len(classification_results_list)
+            # filter bad questions 
+            valid_bad_questions = filter_bad_questions(bad_questions_list, classification_results_list)
+            # print('###'*10)
+            # print(valid_bad_questions)
+
+            # store results 
+            all_turn_results[tr_file].append(valid_bad_questions)
+
+    # store valid bad questions
+    with open('valid_bad_questions.json', 'w') as outfile:
+        json.dump(all_turn_results, outfile, indent=4)
     
 
 
